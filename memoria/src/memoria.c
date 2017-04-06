d8576d137d8a70e0d9b6a6a33f51da75142a8636
Hernan Canzonetta <hcanzonetta@gmail.com>
2017-04-05 23:45:15 -0300

    Agrego proceso Memoria

---
+++ memoria/src/memoria.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : memoria.c
+ Author      : Hernan Canzonetta
+ Version     :
+ Copyright   : Copyright
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Holis"); /* prints Holis */
+	return EXIT_SUCCESS;
+}
