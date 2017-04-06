0ac27440ea261a8b52390e1eafb3a7627936c0d2
Hernan Canzonetta <hcanzonetta@gmail.com>
2017-04-05 23:38:42 -0300

    Agrego proyecto Consola

---
+++ consola/src/consola.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : consola.c
+ Author      : Hernan Canzonetta
+ Version     :
+ Copyright   : Copyright
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <commons/config.h>
+
+int main(void) {
+	puts("Holis"); /* prints Holis */
+	return EXIT_SUCCESS;
+}
