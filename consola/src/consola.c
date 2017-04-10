e6039efbb9acffe327d38aca1423debcb04d2055
German Gruschka <gegruschka@gmail.com>
2017-04-09 21:13:52 -0300

    Prueba consola

--- consola/src/consola.c
+++ consola/src/consola.c
@@ -11,8 +11,97 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <commons/config.h>
+#include <pthread.h>
+#include <string.h>
+
+
+
+
 
 int main(void) {
-	puts("Holis"); /* prints Holis */
-	return EXIT_SUCCESS;
+
+	showMenu();
+
+}
+
+
+void showMenu(){
+
+	int menuopt;
+	printf("\nConsole Menu:\n");
+	printf("1-Start Program\n2-End Program\n3-Disconnect Program\n"
+			"4-Clear Console\n");
+	scanf("%d",&menuopt);
+	char program[50];
+
+
+	switch(menuopt){
+	case 1:
+		requestFilePath(program);//Save in program the file path of the file
+		startProgram(program);
+		showMenu(); //Una vez que se inicia un programa, vuelve a mostrar menu
+		break;
+
+	case 2:
+		endProgram();
+		break;
+
+	case 3:
+		disconnectProgram();
+		break;
+
+	case 4:
+		clearConsole();
+		break;
+
+	case 5:
+
+		break;
+
+	default:
+		printf("Invalid input\n");
+		break;
+
+
+	}
+
+}
+void startProgram(char * programPath){
+
+	printf("\nInitiating:%s\n", programPath);
+
+	//Thread ID
+	pthread_t threadId;
+
+	//Create thread attributes
+	pthread_attr_t attr;
+	pthread_attr_init(&attr);
+
+	pthread_create(&threadId, &attr,  , NULL);
+
+	pthread_join(threadId, NULL);  //NULL means that it isn't going to catch the return value from pthread_exit
+
+
+
+
+}
+void endProgram(){
+	printf("\nEnd Program\n");
+
+}
+void disconnectProgram(){
+	printf("\nDisconnect Program\n");
+
+}
+void clearConsole(){
+	printf("\Clear console\n");
+
+}
+void requestFilePath(char * filePath){
+
+	printf("\nPlease provide file path\n");
+	getchar();
+	gets(filePath);
+	//puts(filePath);
+
 }
---
+++ consola/src/consola.h
@@ -0,0 +1,18 @@
+/*
+ * consola.h
+ *
+ *  Created on: 9/4/2017
+ *      Author: utnso
+ */
+
+#ifndef CONSOLA_H_
+#define CONSOLA_H_
+
+
+void showMenu();
+void startProgram(char program);
+void endProgram();
+void disconnectProgram();
+void clearConsole();
+
+#endif /* CONSOLA_H_ */
