c215a2e92945808dd3b04fee3d620fc4d0642034
Guillermo Gruschka <ggruschka@gmail.com>
2017-06-26 02:32:58 -0300

    cerealizar y decerealizar

--- shared-leto/pcb/pcb.h
+++ shared-leto/pcb/pcb.h
@@ -11,8 +11,25 @@
 #include <commons/collections/list.h>
 #include <stdint.h>
 
+typedef enum{
+	VARIABLE,
+	ARGUMENT,
+}t_variableAdd;
+
+typedef struct t_PCBVariableSize {
+	uint32_t stackIndexRecordCount;
+	uint32_t stackVariableCount;
+	uint32_t stackArgumentCount;
+} t_PCBVariableSize;
+
+typedef struct t_memoryDirection {
+	uint32_t page;
+	uint32_t offset;
+	uint32_t size;
+} t_memoryDirection;
+
 typedef struct t_stackVariable {
-	char *id;
+	char id;
 	uint32_t page;
 	uint32_t offset;
 	uint32_t size;
@@ -22,7 +39,7 @@ typedef struct t_stackIndexRecord {
 	t_list *arguments;
 	t_list *variables;
 	uint32_t returnPosition; // store IP to return
-	uint32_t returnVariable; // mem dir for return variable
+	t_memoryDirection returnVariable; // mem dir for return variable
 } t_stackIndexRecord;
 
 
@@ -32,10 +49,16 @@ typedef struct t_PCB {
 	int sp;	// Stack Pointer
 	int ec;	// Exit Code
 	t_list *stackIndex;
+	t_PCBVariableSize variableSize;
 	void *filesTable; // Puntero a la tabla de archivos del proceso
 } t_PCB;
 
-t_PCB pcb_createDummy(int32_t processID, int32_t programCounter, int32_t StackPointer,int32_t ExitCode);
-
+t_PCB *pcb_createDummy(int32_t processID, int32_t programCounter, int32_t StackPointer,int32_t ExitCode);
+void pcb_addStackIndexRecord(t_PCB *PCB, t_stackIndexRecord *record);
+void pcb_addStackIndexVariable(t_PCB *PCB, t_stackIndexRecord *stackIndex, t_stackVariable *variable, t_variableAdd type);
+uint32_t pcb_getPCBSize(t_PCB *PCB);
+void *pcb_serializePCB(t_PCB *PCB);
+t_PCB *pcb_deSerializePCB(void *serializedPCB, int totalSize, int stackIndexRecordCount, int stackIndexVariableCount, int stackIndexArgumentCount);
+void pcb_dump(t_PCB *PCB);
 
 #endif /* PCB_H_ */
