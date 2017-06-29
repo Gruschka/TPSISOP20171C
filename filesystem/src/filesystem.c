098d66138f4ee503ba8ecce40f51385b9c310c34
Guillermo Gruschka <guillermo.gruschka@gmail.com>
2017-06-29 01:02:21 -0300

    FEDE SOLO ME FALTAS TU

--- filesystem/src/filesystem.c
+++ filesystem/src/filesystem.c
@@ -11,8 +11,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <commons/config.h>
+#include <commons/log.h>
 
-int main(void) {
+t_config *config;
+t_log *logger;
+
+int main(int argc, char **argv) {
+	char *logFile = tmpnam(NULL );
+
+	logger = log_create(logFile, "KERNEL", 1, LOG_LEVEL_DEBUG);
+
+	if (argc < 2) {
+		log_error(logger, "Falta pasar la ruta del archivo de configuracion");
+		return EXIT_FAILURE;
+	} else {
+		config = config_create(argv[1]);
+	}
 	puts("Holis"); /* prints Holis */
 	return EXIT_SUCCESS;
 }
