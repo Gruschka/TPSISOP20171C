e54fe498674f832f4a9aa9b058e35d7331b97569
Hernan Canzonetta <hcanzonetta@gmail.com>
2017-07-07 19:14:02 -0300

    Fix

--- kernel/src/scheduling.h
+++ kernel/src/scheduling.h
@@ -23,7 +23,7 @@ typedef enum t_scheduling_algorithm {
 t_queue *newQueue;
 t_list *readyQueue;
 t_queue *exitQueue;
-t_queue *blockQueue;
+t_list *blockQueue;
 t_list *execList;
 
 //Mutexes
@@ -54,7 +54,9 @@ t_PCB *readyQueue_popProcess();
 t_queue *exitQueue_create();
 void exitQueue_addProcess(t_PCB *process);
 
-t_queue *blockQueue_create();
+t_list *blockQueue_create();
+void blockQueue_addProcess(t_PCB *pcb);
+t_PCB *blockQueue_popProcess(int pid);
 
 t_list *execList_create();
 void execList_addProcess(t_PCB *process);
