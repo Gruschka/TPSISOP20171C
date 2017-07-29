420b55a3702d5fad077f7808658ffabeb389266d
Guillermo Gruschka <guillermo.gruschka@gmail.com>
2017-07-29 12:07:10 -0300

    UASE UASE UASE

--- filesystem/src/filesystem.c
+++ filesystem/src/filesystem.c
@@ -1111,15 +1111,15 @@ int main(int argc, char **argv) {
 	fs_validateFile("/prueba1.bin");
 
 
-	char *bafer = string_new();
-	string_append(&bafer,"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc mi mauris, suscipit euismod leo vitae, tempor sagittis elit nullam.");
-	fs_writeFile("/mnt/SADICA_FS/Archivos/test/prueba1.bin",0,strlen(bafer),bafer);
-	char *read = fs_readFile("/mnt/SADICA_FS/Archivos/test/prueba1.bin", 0, 64);
-	puts(read);
-	free(read);
-	read = fs_readFile("/mnt/SADICA_FS/Archivos/test/prueba1.bin", 60, 68);
-	free(read);
-    //fs_removeFile("/mnt/SADICA_FS/Archivos/test/prueba1.bin");
+//	char *bafer = string_new();
+//	string_append(&bafer,"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc mi mauris, suscipit euismod leo vitae, tempor sagittis elit nullam.");
+//	fs_writeFile("/mnt/SADICA_FS/Archivos/test/prueba1.bin",0,strlen(bafer),bafer);
+//	char *read = fs_readFile("/mnt/SADICA_FS/Archivos/test/prueba1.bin", 0, 64);
+//	puts(read);
+//	free(read);
+//	read = fs_readFile("/mnt/SADICA_FS/Archivos/test/prueba1.bin", 60, 68);
+//	free(read);
+//    fs_removeFile("/mnt/SADICA_FS/Archivos/test/prueba1.bin");
 
 	return EXIT_SUCCESS;
 
